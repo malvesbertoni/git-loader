66cb5ab955787ca8a2e953b792e1c0dcd6c00d80
malvesbertoni <malvesbertoni@gmail.com>
2020-05-02 17:25:51 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+import './styles.css';
+
+export default function App() {
+  const [repositories, setRepositories]= useState([]);
+  const [username, setUsername] = useState(["malvesbertoni"]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`https://api.github.com/users/${username}/repos`);
+      const data = await response.json();
+
+      setRepositories(data);
+    } fetchData()
+  }, []);
+
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+
+    document.title = `You have ${filtered.length} favorite repos`;
+  }, [repositories]);
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? {... repo, favorite: !repo.favorite} : repo
+    });
+
+    setRepositories(newRepositories);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="header">
+        <h1>GIT LOADER</h1>
+        <input type="text" className="input" name="username" placeholder="Type in a github username"
+            onChange={e => setUsername(e.target.value)} />
+      </div>
+      <div className="content-container">
+        <p>Showing {username}'s repositories</p>
+        <ul>
+            {repositories.map(repo => (
+              <li key={repo.id}>
+                { repo.name }
+                { repo.favorite && <span className="favoriteTag"> (FAVORITE!) </span>}
+                <button className="favoriteButton" onClick={() => handleFavorite(repo.id)}> Favorite this repo </button>
+              </li>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 }
-
-export default App;
